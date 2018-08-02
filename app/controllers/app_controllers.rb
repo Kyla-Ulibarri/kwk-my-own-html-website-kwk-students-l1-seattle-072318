ab70e7d44678372d8ee3fe6a180cdf67f8eeecb3
Kyla Ulibarri <k.barri.071@gmail.com>
2018-08-02 16:15:41 +0000

    automatically backed up by learn

--- app/controllers/app_controllers.rb
+++ app/controllers/app_controllers.rb
@@ -10,4 +10,26 @@ class AppController < Sinatra::Base
   get '/' do 
     erb :index
   end 
+  post '/' do
+    @the_result
 end 
+
+require './config/environment'
+require './app/models/sample_model'
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+    return erb :index
+  end
+  
+  post '/' do
+    @the_fortune = get_fortune
+    @the_user = params[:user]
+    return erb :results
+  end
+end
